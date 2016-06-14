b73ebdba83d1561d7748d42be7edd158992ab6f6
mykhailo ivashko <maivashko@gmail.com>
2016-06-14 07:38:26 +0300

    delete coments

--- src/main/java/com/mivashko/movieland/dao/impl/mapper/MovieRowMapper.java
+++ src/main/java/com/mivashko/movieland/dao/impl/mapper/MovieRowMapper.java
@@ -20,28 +20,6 @@ public class MovieRowMapper implements RowMapper<Movie>{
         movie.setRating(resultSet.getDouble("rating"));
         String genres = resultSet.getString("genres");
         movie.setGenres(Arrays.asList(genres.split(", ")));
-
-/*        movie.setId(resultSet.getInt("id"));
-        movie.setName(resultSet.getString("name"));
-        movie.setNameOriginal(resultSet.getString("name_original"));
-        movie.setReleaseYear(resultSet.getInt("release_year"));
-        movie.setDescription(resultSet.getString("description"));
-        movie.setRating(resultSet.getDouble("rating"));
-        movie.setPrice(resultSet.getDouble("price"));
-        String genres = resultSet.getString("genres");
-        List<String> genresList = new ArrayList<String>();
-        String[] genresArray = genres.split(", ");
-        for (String s : genresArray) {
-            genresList.add(s);
-        }
-        movie.setGenres(genresList);
-        String countries = resultSet.getString("countries");
-        List<String> countriesList = new ArrayList<String>();
-        String[] countriesArray = countries.split(", ");
-        for (String s : countriesArray) {
-            countriesList.add(s);
-        }
-        movie.setCountries(countriesList);*/
-        return movie;
+       return movie;
     }
 }
